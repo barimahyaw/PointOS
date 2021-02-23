d2be7494bfc8545b2f9f96139ac2c20fe1e49ecd
ntiri boateng <ntiriboateng@outlook.com>
2021-02-23 10:58:05 +0000

    Initial commit - master will make the necessary changes

---
+++ PointOS.DataAccess/Entities/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOS.DataAccess.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
