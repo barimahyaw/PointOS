85dcfa873d1b7ed024323da854089ac743430951
Isaac Nana Gyan Ayeh <iayeh@sltf.gov.gh>
2021-04-26 17:55:59 +0000

    Api controller for product pricing initial implementation

---
+++ PointOS.Api/Controllers/v1/PricingController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PointOS.BusinessLogic.Interfaces;
+using PointOS.Common.DTO.Request;
+using PointOS.Common.DTO.Response;
+using PointOS.Common.Enums;
+using System.Threading.Tasks;
+
+namespace PointOS.Api.Controllers.v1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PricingController : ControllerBase
+    {
+        private readonly IProductPricingBusiness _productPricingBusiness;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productPricingBusiness"></param>
+        public PricingController(IProductPricingBusiness productPricingBusiness)
+        {
+            _productPricingBusiness = productPricingBusiness;
+        }
+
+        /// <summary>
+        /// Saves a product pricing record
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ResponseHeader> Post(ProductPricingRequest request)
+        {
+            request.Operation = CrudOperation.Create;
+            return await _productPricingBusiness.SaveAsync(request);
+        }
+
+        /// <summary>
+        /// Updates a product pricing record
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<ResponseHeader> Put(ProductPricingRequest request)
+        {
+            request.Operation = CrudOperation.Update;
+            return await _productPricingBusiness.SaveAsync(request);
+        }
+    }
+}
