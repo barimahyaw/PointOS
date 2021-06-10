9bceab2ce2d7135dfac0c6893afeeed90a72153e
Isaac Nana Gyan Ayeh <iayeh@sltf.gov.gh>
2021-06-10 16:57:12 +0000

    Subscription entities added

---
+++ PointOS.DataAccess/Entities/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PointOS.DataAccess.Entities
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public Guid GuidId { get; set; }
+        public string Token { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+        public int SubscriptionTypeId { get; set; }
+        public SubscriptionType SubscriptionType { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string CreatedUserId { get; set; }
+        public ApplicationUser CreatedUser { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ PointOS.DataAccess/Entities/SubscriptionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointOS.DataAccess.Entities
+{
+    public class SubscriptionType
+    {
+        public int Id { get; set; }
+        public Guid GuidId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
