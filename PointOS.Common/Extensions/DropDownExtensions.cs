4e5bdccf9f3d4b5d0ea07955504e81192b363d56
Isaac Nana Gyan Ayeh <iayeh@sltf.gov.gh>
2021-08-09 16:08:25 +0000

    Implemented Currency Dropdown extention method

--- PointOS.Common/Extensions/DropDownExtensions.cs
+++ PointOS.Common/Extensions/DropDownExtensions.cs
@@ -48,18 +48,47 @@ namespace PointOS.Common.Extensions
             return items;
         }
 
+        /// <summary>
+        /// Dropdown extension method for product which has productId as value 
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="selected"></param>
+        /// <returns></returns>
         public static IEnumerable<SelectListItem> GetProduct(
             this IEnumerable<ProductResponse> product, int selected)
         {
-            var items = product.OrderBy(p => p.Id).Select(p => new SelectListItem
-            {
-                Selected = p.Id == selected,
-                Text = p.Name,
-                Value = p.Id.ToString()
-            })
+            var items = product.OrderBy(p => p.Id)
+                .Select(p => new SelectListItem
+                {
+                    Selected = p.Id == selected,
+                    Text = p.Name,
+                    Value = p.Id.ToString()
+                })
                 .ToList();
 
             items.Insert(0, new SelectListItem { Text = "-- Select Product --", Value = "", Selected = true });
+
+            return items;
+        }
+
+        /// <summary>
+        /// Dropdown extension method for Currency
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <param name="selected"></param>
+        /// <returns></returns>
+        public static IEnumerable<SelectListItem> GetCurrencies(this IEnumerable<CurrencyResponse> currency,
+            int selected)
+        {
+            var items = currency.OrderBy(c => c.Id)
+                .Select(c => new SelectListItem
+                {
+                    Selected = c.Id == selected,
+                    Text = c.Currency,
+                    Value = c.Id.ToString()
+                }).ToList();
+
+            items.Insert(0, new SelectListItem { Text = "-- Select Currency --", Value = "", Selected = true });
 
             return items;
         }
